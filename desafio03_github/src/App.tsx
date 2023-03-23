2181c35bcb1a8534a18defd4df47c3dccce75021
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-03-23 20:25:48 -0300

    Criando o contextIssues e preparando a call do github

--- desafio03_github/src/App.tsx
+++ desafio03_github/src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header";
 import { IssueCard } from "./components/IssueCard";
 import { MainTopCard } from "./components/MainTopCard";
 import { SearchForm } from "./components/SearchForm";
+import { RepoIssuesProvider } from "./context/TransactionContext";
 import { Router } from "./Router";
 import { GlobalStyle, MainContent } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
@@ -15,17 +16,10 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
     <BrowserRouter> 
+    <RepoIssuesProvider>
     <Router />
-   {/** <MainContent>
+    </RepoIssuesProvider>
 
-    <MainTopCard/>
-
-    <SearchForm/> 
-
-    <IssueCard/>
-
-
-    </MainContent> */}
     <GlobalStyle />
     </BrowserRouter>
     </ThemeProvider>
